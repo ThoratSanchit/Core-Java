1e0fde9277d0786875eed59601b587a4cd3494c3
Sanchit_Thorat <sanchitcode77@gmail.com>
2025-02-20 12:13:14 +0530

    new commit

--- CollectionFramwork/ArrayList/MyArrayList.java
+++ CollectionFramwork/ArrayList/MyArrayList.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
 public class MyArrayList {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList();
-
+        ArrayList s=new ArrayList<>();
+        s.add("sa");
+        s.add(2);
+        System.out.println(s);
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i <= 2; i++) {
             System.out.println("Enter the name: " + i + " ");
